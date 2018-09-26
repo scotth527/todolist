f386a14b9c29cb170c938a36ea9feab3a3508932
Scott Huang <scottyh527@gmail.com>
2018-09-26 22:54:01 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# Hello World with React and Cloud 9
+
+Requierents: Make sure you have node version 8
+
+##### Download the boilerplate using the BreatheCode CLI
+```
+$ npm i breathecode-cli -g
+```
+
+##### Download the boilerplate using the BreatheCode CLI
+```
+$ breathecode start:react-project -r
+```
+##### and install the npm package:
+```
+$ npm install
+```
+
+## Start coding! 
+
+Start the webpack server with live reload:
+- `$ npm run c9` for Cloud 9 Users.
+- `$ npm run dev-server` for windows, mac or linux.
+
+You can update the `styles/index.scss` or `js/index.js` depending on your needs.
+Add more files into your, `./src/js/components` or styles folder as you need them.
+
+## Publish your website! 
+
+This boilerplate is 100% compatible with the free github pages hosting.
+To publish your website you need to push your code to your github repository and run the following command after:
+```sh
+$ npm run deploy
+```
+Note: You will need to [configure github pages for the branch gh-pages](https://help.github.com/articles/configuring-a-publishing-source-for-github-pages/#enabling-github-pages-to-publish-your-site-from-master-or-gh-pages)
---
+++ src/js/component/home.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+
+//include images into your bundle
+import rigoImage from '../../img/rigo-baby.jpg';
+
+//create your first component
+export class Home extends React.Component{
+    
+    constructor(){
+        super();
+        this.state = {
+            list: [],
+            currentTask: ""
+        };
+        
+        
+    }
+    
+    updateList(task) {
+        let currentState = this.state;
+        currentState.list.push(task);
+        currentState.currentTask = "";
+        this.setState(currentState);
+    }
+    
+    inputTask (thing) {
+        let currentState = this.state;
+        currentState.currentTask = thing;
+        this.setState(currentState);
+    }
+    
+    
+    
+    
+    deleteTask (index) {
+        let currentState = this.state;
+        currentState.list.splice(index,1);
+        this.setState(currentState);
+    }
+    
+    
+    
+    render(){
+        
+        
+        return (
+            <div className="text-center mt-2">
+                <div className="container border border-dark">
+                    <h1>todos</h1>
+                    <div className="row">
+                        <div className="col-8 mx-auto">
+                            <input onChange={(e) => this.inputTask(e.target.value)} 
+                            onKeyPress={(e) => {if(e.key === "Enter") 
+                            { this.updateList(e.target.value);}  }} 
+                            value={this.state.currentTask}
+                            className="col-11" type="text" placeholder="What needs to be done?">
+                            </input>
+                            <ul className="list-inline ">
+                           
+                                {
+                                        this.state.list.map((garbage, index) => {
+                                        return <li key={index} className="list-inline-item d-flex justify-content-between">{garbage} 
+                                            <span onClick={(index) => {this.deleteTask(index);}} position={index}>
+                                                <i className="fas fa-times"></i>
+                                            </span></li>;
+                                        }
+                                    )
+                                }
+                            </ul>
+                            {this.state.list.length} items left
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
